f8523e92027522d69a31a983475bbdccd80fc16e
Robert <robert2005n@gmail.com>
2025-05-18 13:26:46 +0300

    full work without load_save and design

--- javascript.js
+++ javascript.js
@@ -1,2 +1,326 @@
-const header = document.querySelector("h1")
-header.textContent = "Hello, my world!"
+const body = document.querySelector("body");
+const settings = document.querySelector(".settings");
+const settingsWidjet = document.querySelector(".settingsWidjet");
+const displayHeight = document.querySelector("#displayHeight");
+const displayWidth = document.querySelector("#displayWidth");
+const displayHeightValue = document.querySelector(".displayHeightValue");
+const displayWidthValue = document.querySelector(".displayWidthValue");
+const choseDisplayResButton = document.querySelector("#choseDisplayResButton");
+const header = document.querySelector("h1");
+const display = document.querySelector(".display");
+const clear = document.querySelector("#clear");
+const bucket = document.querySelector("#bucket");
+const pipette = document.querySelector("#pipette");
+const brush = document.querySelector("#brush");
+const remover = document.querySelector("#remover");
+const undo = document.querySelector("#undo");
+const redo = document.querySelector("#redo");
+const palet = document.querySelector("#palette");
+const redSlider = document.querySelector("#red");
+const greenSlider = document.querySelector("#green");
+const blueSlider = document.querySelector("#blue");
+const choseButton = document.querySelector(".choseButton");
+const chousenColor = document.querySelector(".chousenColor");
+const paleteWidjet = document.querySelector(".paleteWidjet");
+display.addEventListener("mouseup", () => {mousehold = false});
+display.addEventListener("mouseleave", () => {mousehold = false});
+
+let selectedTool = "brush";
+
+let displayRes = [64,64];
+let currentDisplay = [];
+let currentDisplayColor = [];
+let eventSequencePos =	-1;
+let eventSequence = [];
+let background_color = "white";
+let mainColor = "black";
+let mousehold = false;
+let clearEvent = new Event('clearPixel');
+
+function displayEvents() {
+	display.addEventListener('clearPixel', () => {
+		if (eventSequence.length - 1 > eventSequencePos) {
+			for (let i = eventSequence.length - 1; i > eventSequencePos; i--){
+				eventSequence.splice(i,1);
+			}
+		}
+		for (let i = 0; i < displayRes[0]; i++){
+			for (let j = 0; j < displayRes[1]; j++){
+				currentDisplay[i][j].style.backgroundColor = background_color;
+				currentDisplayColor.push([i,j,background_color]);
+
+			}
+		}
+		if (JSON.stringify(eventSequence[eventSequencePos]) != JSON.stringify(currentDisplayColor)) {
+			console.log(eventSequence[eventSequencePos], currentDisplayColor)
+			eventSequence.push(currentDisplayColor); eventSequencePos += 1;
+			console.log("currentDisplayColor was created", eventSequencePos);
+		}
+		currentDisplayColor = [];
+
+	});
+}
+
+function painDisplay(eventSequencePos) {
+	beforeColorDisplay = eventSequence[eventSequencePos];
+	for (let i = 0; i < displayRes[0]*displayRes[1]; i++){
+		currentDisplay[beforeColorDisplay[i][0]][beforeColorDisplay[i][1]].style.backgroundColor = beforeColorDisplay[i][2];
+	}
+
+}
+
+function createPixel(i,j) {
+	const pixel = document.createElement("div");
+	pixel.setAttribute("id",`${i+1};${j+1}`);
+	pixel.setAttribute("style",`background-color: ${background_color}; flex: 1 0 auto; aspect-ratio: 1/1; user-select: none; -webkit-user-drag: none;`);
+	pixel.addEventListener("dragstart", (e) => {
+		e.preventDefault()
+	});
+	pixel.addEventListener("mousedown", () => {mousehold = true});
+	pixel.addEventListener("mouseup", () => {mousehold = false});
+	return pixel;
+}
+
+function brushEventsPixel(pixel) {
+	pixel.addEventListener("mouseenter", () => {
+		if (mousehold === true && selectedTool === "brush") {
+			pixel.style.backgroundColor = mainColor;
+		}
+	});
+	pixel.addEventListener("mousedown", () => {
+		if (selectedTool === "brush"){
+			pixel.style.backgroundColor = mainColor;
+		}
+	});
+}
+
+function removerEventsPixel (pixel) {
+	pixel.addEventListener("mouseenter", () => {
+		if (mousehold === true && selectedTool === "remover") {
+			pixel.style.backgroundColor = background_color;
+		}
+	});
+	pixel.addEventListener("click", () => {
+		if (selectedTool === "remover") {
+			pixel.style.backgroundColor = background_color;
+		}
+	});
+}
+
+function pipetteEventPixel(pixel) {
+	pixel.addEventListener("click", () => {
+		if (selectedTool === "pipette"){
+			mainColor = pixel.style.backgroundColor;
+		}
+	});
+}
+
+function bucketEventsPixel(pixel) {	
+	pixel.addEventListener("mousedown", () => {
+		if (selectedTool === "bucket") {
+			const row = currentDisplay.findIndex(arr => arr.includes(pixel));
+			const col = currentDisplay[row].indexOf(pixel);
+			const beforeColor = pixel.style.backgroundColor;
+			console.log(currentDisplay[row][col],beforeColor);
+			bucketBrill(row,col,mainColor);
+		}
+	});
+}
+
+function bucketBrill(i, j, newColor) {
+	let pixelQueue = [];
+	let changedPixels = new Set();
+	const origignalColor = currentDisplay[i][j].style.backgroundColor;
+	console.log(origignalColor,"here",currentDisplay[i][j],i,j);
+	if (origignalColor === newColor) {
+		return;
+	}
+	pixelQueue.push([i,j]);
+	console.log(i,j);
+
+	while (pixelQueue.length != 0) {
+		let currentPixelCords = pixelQueue.shift();
+		i = currentPixelCords[0];
+		j = currentPixelCords[1];
+
+		currentDisplay[i][j].style.backgroundColor = newColor;
+		changedPixels.add(`${i};${j}`);
+
+		if (i-1 >= 0 && currentDisplay[i-1][j].style.backgroundColor === origignalColor && !changedPixels.has(`${i-1};${j}`)) {
+			pixelQueue.push([i-1,j]);
+			changedPixels.add(`${i-1};${j}`);
+		} 
+		if (j+1 < displayRes[1] && currentDisplay[i][j+1].style.backgroundColor === origignalColor && !changedPixels.has(`${i};${j+1}`)) {
+			pixelQueue.push([i,j+1]);
+			changedPixels.add(`${i};${j+1}`);
+		}
+		if (j-1 >= 0 && currentDisplay[i][j-1].style.backgroundColor === origignalColor && !changedPixels.has(`${i};${j-1}`)) {
+			pixelQueue.push([i,j-1]);
+			changedPixels.add(`${i};${j-1}`);
+		}
+		if (i+1 < displayRes[0] && currentDisplay[i+1][j].style.backgroundColor === origignalColor && !changedPixels.has(`${i+1};${j}`)) {
+			pixelQueue.push([i+1,j]);
+			changedPixels.add(`${i+1};${j}`);
+		}
+	}
+}
+
+function displayConstructor(displayRes) {
+	display.innerHTML = "";
+	selectedTool = "brush";
+	currentDisplay = [];
+	currentDisplayColor = [];
+	eventSequencePos = -1;
+	eventSequence = [];
+	background_color = "white";
+	mainColor = "black";
+	mousehold = false;
+
+	for (let i = 0; i < displayRes[0]; i++){
+		for (let j = 0; j < displayRes[1]; j++){
+			currentDisplayColor.push([i,j,"white"]);
+		}
+	} eventSequence.push(currentDisplayColor); eventSequencePos += 1;
+	currentDisplayColor = [];
+	for(let i = 0; i < displayRes[0]; i++){
+		const currentRow = [];
+		const row = document.createElement('div');
+		row.setAttribute("style","display: flex;")
+		row.setAttribute("dragable", false);
+		row.addEventListener("mouseup", () => {mousehold = false});
+		currentDisplay.push(currentRow);
+		display.append(row);
+		for(let j = 0; j < displayRes[1]; j++){
+			const pixel = createPixel(i,j);
+			currentRow.push(pixel);
+			row.append(pixel);
+			brushEventsPixel(pixel);
+			bucketEventsPixel(pixel);
+			removerEventsPixel(pixel);
+			pipetteEventPixel(pixel);
+			pixel.addEventListener("mouseup", () => {
+				if (eventSequence.length - 1 > eventSequencePos) {
+					for (let i = eventSequence.length - 1; i > eventSequencePos; i--){
+						eventSequence.splice(i,1);
+					}
+				}
+				for (let i = 0; i < displayRes[0]; i++){
+					for (let j = 0; j < displayRes[1]; j++){
+						currentDisplayColor.push([i,j,currentDisplay[i][j].style.backgroundColor]);
+					}
+				}
+				if (JSON.stringify(eventSequence[eventSequencePos]) != JSON.stringify(currentDisplayColor)) {
+					eventSequence.push(currentDisplayColor); eventSequencePos += 1;
+					console.log("currentDisplayColor was created", eventSequencePos);
+				}
+				currentDisplayColor = [];
+			});
+		}	
+	} displayEvents();
+}
+
+let settingsWidjetOpen = false;
+settings.addEventListener('click', () => {
+	if (settingsWidjetOpen){
+		settingsWidjet.style.display = "none";
+		settingsWidjetOpen = false;
+	} else {
+		settingsWidjet.style.display = "flex";
+		settingsWidjetOpen = true;
+	}
+});
+
+displayHeight.addEventListener('mousemove', (e) => {
+	console.log(displayHeight.value);
+	displayHeightValue.textContent = displayHeight.value;
+});
+
+displayWidth.addEventListener('mousemove', (e) => {
+	console.log(displayWidth.value);
+	displayWidthValue.textContent = displayWidth.value;
+});
+
+choseDisplayResButton.addEventListener('click', () => {
+	displayRes[0] = displayHeight.value;
+	displayRes[1] = displayWidth.value;
+	settingsWidjet.style.display = "none";
+	displayConstructor(displayRes);
+});
+
+clear.addEventListener('click', () => {
+	display.dispatchEvent(clearEvent);	
+});
+
+brush.addEventListener('click', () => {
+	selectedTool = "brush";
+});
+
+bucket.addEventListener('click', () => {
+	selectedTool = "bucket";
+});
+
+pipette.addEventListener('click', () => {
+	selectedTool = "pipette";
+});
+
+remover.addEventListener('click', () => {
+	selectedTool = "remover";
+});
+
+undo.addEventListener('click', () => {
+	if(eventSequencePos > 0) {
+		eventSequencePos -= 1;
+		painDisplay(eventSequencePos);
+	}
+});
+
+redo.addEventListener('click', () => {
+	if (eventSequencePos < eventSequence.length -1) {
+		eventSequencePos += 1;
+		painDisplay(eventSequencePos);
+	}
+});
+
+let paleteWidjetOpen = false;
+palet.addEventListener('click', () => {
+	if (paleteWidjetOpen){
+		paleteWidjet.style.display = "none";
+		paleteWidjetOpen = false;
+	} else {
+		paleteWidjet.style.display = "flex";
+		paleteWidjetOpen = true;
+		chousenColor.dispatchEvent(sliderValueChanged);
+	}	
+});
+
+choseButton.addEventListener('click', () => {
+	paleteWidjet.style.display = "none";
+	chousenColor.dispatchEvent(sliderValueChanged);
+});
+
+redSlider.addEventListener('mousemove', (e) => {
+	console.log(redSlider.value);
+	chousenColor.dispatchEvent(sliderValueChanged);
+});
+
+greenSlider.addEventListener('mousemove', (e) => {
+	console.log(greenSlider.value);
+	chousenColor.dispatchEvent(sliderValueChanged);
+});
+
+blueSlider.addEventListener('mousemove', (e) => {
+	console.log(blueSlider.value);
+	chousenColor.dispatchEvent(sliderValueChanged);
+});
+
+let sliderValueChanged = new Event("sliderValueChanged");
+chousenColor.addEventListener('sliderValueChanged', () => {
+	chousenColor.setAttribute('style',`background-color: rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value});`);
+});
+
+choseButton.addEventListener('click', () => {
+	mainColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+});
+
+displayConstructor(displayRes);
+
